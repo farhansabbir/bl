0167084d595f0a26a255a5c6db08608c5b7fd86a
Farhan Sabbir Siddique <fsabbir@gmail.com>
2022-10-29 00:44:32 +0600

    Session 2 complete

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from crypt import methods
 from pickle import GET
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return render_template("index.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+    return str(float(request.form["num1"]) + float(request.form["num2"]))
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
