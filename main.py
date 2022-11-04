e79da0e9beec611dac3627938ef10d5d8b525979
Farhan Sabbir Siddique <fsabbir@gmail.com>
2022-11-05 00:41:35 +0600

    new branch added for week2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from crypt import methods
-from pickle import GET
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -11,6 +10,19 @@ def root():
 @app.route("/add", methods=["POST"])
 def add():
     return str(float(request.form["num1"]) + float(request.form["num2"]))
+
+@app.route("/subtract", methods=["POST"])
+def subtract():
+    return str(float(request.form["num1"]) - float(request.form["num2"]))
+
+@app.route("/divide", methods=["POST"])
+def divide():
+    return str(float(request.form["num1"]) / float(request.form["num2"]))
+
+@app.route("/multiply", methods=["POST"])
+def multiply():
+    return str(float(request.form["num1"]) * float(request.form["num2"]))
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
