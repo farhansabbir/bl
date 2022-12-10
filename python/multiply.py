9602c7e2cdeda4cce5af2915806c717c4fbcd569
Farhan Sabbir Siddique <fsabbir@gmail.com>
2022-12-10 23:57:09 +0600

    json responses

--- python/multiply.py
+++ python/multiply.py
@@ -1,12 +1,12 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/multiply", methods=["POST"])
 def subtract():
-    return str(float(request.form["num1"]) * float(request.form["num2"]))
+    return jsonify({"data" : str(float(request.form["num1"]) * float(request.form["num2"]))})
 
 
 if __name__ == "__main__":
-    app.run(port=3002)
+    app.run(port=3000)
--- python/subtract.py
+++ python/subtract.py
@@ -1,12 +1,12 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/subtract", methods=["POST"])
 def subtract():
-    return str(float(request.form["num1"]) - float(request.form["num2"]))
+    return jsonify({"data" : str(float(request.form["num1"]) - float(request.form["num2"]))})
 
 
 if __name__ == "__main__":
-    app.run(port=3001)
+    app.run(port=3000)
