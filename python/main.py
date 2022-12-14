44695569ab64d2466eb10d851c842da54173e46c
Farhan Sabbir Siddique <fsabbir@gmail.com>
2022-12-14 16:37:21 +0000

    Adds dockerfile for add

--- python/main.py
+++ python/main.py
@@ -10,5 +10,5 @@ def root():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run("0.0.0.0",port=3000)
     
--- python/subtract.py
+++ python/subtract.py
@@ -12,4 +12,4 @@ def subtract():
 
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run("0.0.0.0",port=3000)
